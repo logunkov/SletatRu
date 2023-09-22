86e8f652a043aa12f5c8ec1d4e0c9cbf5b90d199
logunkov <logunkovkn@gmail.com>
2023-09-22 14:57:45 +0700

    Add Theme

---
+++ App/SletatRu/Theme/Sizes.swift
@@ -0,0 +1,36 @@
+//
+//  Sizes.swift
+//  SletatRu
+//
+//  Created by Constantin on 06.09.2023.
+//
+
+import Foundation
+
+enum Sizes {
+	enum Medium {
+		static let doublePadding: CGFloat = 32
+		static let padding: CGFloat = 16
+		static let halfPadding: CGFloat = 8
+
+		static let paddingText: CGFloat = 10
+		static let halfPaddingText: CGFloat = 5
+
+		static let cornerRadius: CGFloat = 16
+		static let halfCornerRadius: CGFloat = 8
+
+		static let borderWidth: CGFloat = 1
+		static let multiplier: CGFloat = 0.75
+		static let maxTextCount = 22
+		static let maxTextIndex = 2
+	}
+
+	enum Emoji {
+		static let padding: CGFloat = 16
+		static let cornerRadius: CGFloat = 35
+	}
+
+	enum Alfa {
+		static let button: CGFloat = 0.1
+	}
+}
---
+++ App/SletatRu/Theme/Theme.swift
@@ -0,0 +1,21 @@
+//
+//  Theme.swift
+//  SletatRu
+//
+//  Created by Constantin on 06.09.2023.
+//
+
+import UIKit
+
+enum Theme {
+	static let accentColor = UIColor.systemGray6
+	static let backgroundColor = UIColor.white
+
+	static let white = UIColor.white
+	static let black = UIColor.black
+	static let blue = UIColor.systemBlue
+	static let gray = UIColor.systemGray
+
+	static let rating = UIColor.color(light: UIColor(hex: 0xFFC7_0033), dark: UIColor(hex: 0xFFC7_0033))
+	static let ratingText = UIColor.color(light: UIColor(hex: 0xFFA800), dark: UIColor(hex: 0xFFA800))
+}
